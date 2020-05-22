b85cda20a73864e7fb7b7fe32dd68299abcf3049
Yiran Li <v.yiran.li@gmail.com>
2020-05-21 20:09:09 -0700

    Added button to clear the grid

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 let htmlStyles = window.getComputedStyle(document.querySelector("html"));
 let rowNum = parseInt(htmlStyles.getPropertyValue("--rowNum"));
-let gridContainer = document.querySelector(".sketchGrid");
+const gridContainer = document.querySelector(".sketchGrid");
 
 // document.documentElement.style.setProperty("--rowNum", 6); Sets number of rows
 // document.documentElement.style.setProperty("--colNum", 6);
@@ -10,8 +10,8 @@ function generateGrid(number) {
     let gridTemplate = document.createElement('div');
     gridTemplate.classList.add("gridSquare");
 
-    for (let i = 0; i < rowNum; i++) {
-        for (let j = 0; j < rowNum; j++) {
+    for (let i = 0; i < number; i++) {
+        for (let j = 0; j < number; j++) {
             let gridDiv = gridTemplate.cloneNode(true);
             gridDiv.setAttribute('id', `${i}-${j}`);
             gridContainer.appendChild(gridDiv);
@@ -19,4 +19,17 @@ function generateGrid(number) {
     }
 }
 
-generateGrid();
+function fillSquare() {
+    this.style.backgroundColor = 'blue';
+}
+
+function clearGrid() {
+    gridSquares.forEach(gridSquare => gridSquare.style = '');
+}
+
+generateGrid(rowNum);
+
+document.querySelector("#clear").onclick = () => {clearGrid()};
+
+const gridSquares = document.querySelectorAll(".gridSquare");
+gridSquares.forEach(gridSquare => gridSquare.addEventListener('mouseenter', fillSquare));
